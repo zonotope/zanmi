7a6cb230650ee7b6803500312b991e4c877397c2
ben lamothe <zonotope@gmail.com>
2016-07-20 21:29:18 -0400

    add database protocol implementation for postgres

--- src/zanmi/boundary/database.clj
+++ src/zanmi/boundary/database.clj
@@ -4,7 +4,6 @@
   "Interact with a database"
   (create-database! [db] "create the database specified by `db`")
   (drop-database!   [db] "drop the database specified by `db`")
-
   (create! [db attrs]    "save the new profile described in `attrs`")
   (get     [db username] "get the profile for `username`")
   (update! [db attrs]    "update the saved profile described in `attrs`")
--- src/zanmi/component/database.clj
+++ src/zanmi/component/database.clj
@@ -1,18 +1,42 @@
 (ns zanmi.component.database
   (:require [zanmi.boundary.database :as database]
             [postgres-component.core :as postgres :refer [postgres]]
-            [hikari-cp.core :refer [make-datasource close-datasource]]
+            [honeysql.core :as sql]
+            [honeysql.helpers :refer [delete-from from select sset update
+                                      where]]
             [clojure.java.jdbc :as jdbc]
             [com.stuartsierra.component :as component])
   (:import [postgres_component.core Postgres]))
 
-(extend-protocol database/Database
-  Postgres
-  (create-database! [db]
-    (postgres/create-database! db))
 
-  (drop-database! [db]
-    (postgres/drop-database! db)))
+(let [pg-table :profiles]
+  (extend-protocol database/Database
+    Postgres
+    (create-database! [db]
+      (postgres/create-database! db))
+
+    (drop-database! [db]
+      (postgres/drop-database! db))
+
+    (create! [{db-spec :spec} attrs]
+      (jdbc/insert! pg-table db-spec attrs))
+
+    (update! [{db-spec :spec} username attrs]
+      (jdbc/query db-spec (-> (update pg-table)
+                              (sset attrs)
+                              (where := :username username)
+                              (sql/format))))
+
+    (get [{db-spec :spec} username]
+      (jdbc/query db-spec (-> (select :*)
+                              (from  pg-table)
+                              (where := :username username)
+                              (sql/format))))
+
+    (delete! [{db-spec :spec} username]
+      (jdbc/query db-spec (-> (delete-from pg-table)
+                              (where := :username username)
+                              (sql/format))))))
 
 (defn database [config]
   (postgres config))
