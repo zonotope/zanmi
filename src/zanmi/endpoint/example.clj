1849470c3528b6853eeeb90a511945c37e68cdb5
ben lamothe <zonotope@gmail.com>
2016-07-21 00:38:14 -0400

    remove example endpoint

--- src/zanmi/endpoint/example.clj
+++
@@ -1,7 +0,0 @@
-(ns zanmi.endpoint.example
-  (:require [compojure.core :refer :all]))
-
-(defn example-endpoint [{{db :spec} :db}]
-  (context "/example" []
-    (GET "/" []
-      "This is an example endpoint")))
