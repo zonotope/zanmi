09f9a639b1a6288610e016f8a9e8f42ee20a34f2
ben lamothe <zonotope@gmail.com>
2016-07-20 21:13:16 -0400

    complete database interaction protocol

--- src/zanmi/boundary/database.clj
+++ src/zanmi/boundary/database.clj
@@ -2,5 +2,10 @@
 
 (defprotocol Database
   "Interact with a database"
-  (create-database! [db-config] "create the database specified by `db-config`")
-  (drop-database! [db-config] "drop the database specified by `db-config`"))
+  (create-database! [db] "create the database specified by `db`")
+  (drop-database!   [db] "drop the database specified by `db`")
+
+  (create! [db attrs]    "save the new profile described in `attrs`")
+  (get     [db username] "get the profile for `username`")
+  (update! [db attrs]    "update the saved profile described in `attrs`")
+  (delete! [db username] "remove the profile record for `username`"))
