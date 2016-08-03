065a9c1be84ef008dc9050e0867d1277e222c230
ben lamothe <zonotope@gmail.com>
2016-08-02 22:23:13 -0400

    remove redundant response functions

--- src/zanmi/endpoint/profile.clj
+++ src/zanmi/endpoint/profile.clj
@@ -2,7 +2,8 @@
   (:require [zanmi.profile :refer [get create! delete! update! valid?]]
             [buddy.sign.jwt :as jwt]
             [compojure.core :refer [context DELETE GET PUT POST]]
-            [ring.util.response :as response :refer [content-type response]]))
+            [ring.util.response :as response :refer [created content-type
+                                                     response]]))
 
 (defn- resource-url [username]
   (str "/profiles/" username))
@@ -14,14 +15,6 @@
 
 (defn- profile->token [profile secret]
   {:token (sign-profile profile secret)})
-
-(defn- ok [token]
-  (-> (response token)
-      (content-type "text/json")))
-
-(defn- created [url token]
-  (-> (response/created url token)
-      (content-type "text/json")))
 
 (def ^:private unauthorized
   (-> (response {:message "bad username or password"})
@@ -38,24 +31,28 @@
         (if-let [token (-> (create! db {:username username, :password password})
                            (profile->token secret))]
           (created (resource-url username) token)
+
           conflict))
 
       (GET "/:username" [username password]
         (if-let [profile (valid? db username password)]
           (-> profile
               (profile->token secret)
-              (ok))
+              (response))
+
           unauthorized))
 
       (PUT "/:username" [username password new-password]
         (if (valid? db username password)
           (-> (update! db username new-password)
               (profile->token secret)
-              (ok))
+              (response))
+
           unauthorized))
 
       (DELETE "/:username" [username password]
         (if (valid? db username password)
           (-> (delete! db username)
-              (ok))
+              (response))
+
           unauthorized)))))
