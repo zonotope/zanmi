80d526eb711febe8a4ce608a60e2c5c3b68c9f39
ben lamothe <zonotope@gmail.com>
2016-08-04 10:21:11 -0400

    refactor for clarity

--- src/zanmi/endpoint/profile.clj
+++ src/zanmi/endpoint/profile.clj
@@ -7,13 +7,16 @@
 (defn- resource-url [username]
   (str "/profiles/" username))
 
-(defn- sign-profile [profile secret]
+(defn- sign [profile secret]
   (-> profile
       (select-keys [:id :username])
       (jwt/sign secret)))
 
-(defn- profile->token [profile secret]
-  {:token (sign-profile profile secret)})
+(defn- token-response-body [profile secret]
+  {:token (sign profile secret)})
+
+(defn- ok-token-response [profile secret]
+  (response (token-response-body profile secret)))
 
 (defn- when-authorized [db username password response-fn]
   (if-let [profile (valid? db username password)]
@@ -27,23 +30,21 @@
       (POST "/" [username password]
         (if-let [profile (create! db {:username username, :password password})]
           (created (resource-url username)
-                   (profile->token profile secret))
+                   (token-response-body profile secret))
           (-> (response {:message "username is already taken"})
               (assoc :status 409))))
 
       (GET "/:username" [username password]
         (when-authorized db username password
-                         (fn [profile] (-> profile
-                                          (profile->token secret)
-                                          (response)))))
+                         (fn [profile] (ok-token-response profile secret))))
 
       (PUT "/:username" [username password new-password]
         (when-authorized db username password
-                         (fn [_] (-> (update! db username new-password)
-                                    (profile->token secret)
-                                    (response)))))
+                         (fn [_]
+                           (let [new-profile (update! db username new-password)]
+                             (ok-token-response new-profile secret)))))
 
       (DELETE "/:username" [username password]
         (when-authorized db username password
-                         (fn [_] (-> (delete! db username)
-                                    (response {:message "deleted"}))))))))
+                         (fn [_] (when (delete! db username)
+                                  (response {:message "deleted"}))))))))
