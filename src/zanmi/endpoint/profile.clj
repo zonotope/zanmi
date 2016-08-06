c65618d8a4ad1e0956ab7fca1ac9fe7efe4513b4
ben lamothe <zonotope@gmail.com>
2016-08-06 19:10:49 -0400

    flesh out the view

--- src/zanmi/endpoint/profile.clj
+++ src/zanmi/endpoint/profile.clj
@@ -1,6 +1,7 @@
 (ns zanmi.endpoint.profile
   (:require [zanmi.data.profile :refer [authenticate create! delete! update!]]
-            [zanmi.view.profile :refer [render-message render-token]]
+            [zanmi.view.profile :refer [auth-error create-error deleted-message
+                                        render-message render-token]]
             [compojure.core :refer [context DELETE GET PUT POST]]
             [ring.util.response :refer [created response]]))
 
@@ -15,7 +16,7 @@
 (defn- when-authenticated [db username password response-fn]
   (if-let [profile (authenticate db username password)]
     (response-fn profile)
-    (-> (response (render-message "bad username or password"))
+    (-> (response auth-error)
         (assoc :status 401))))
 
 (defn profile-endpoint [secret]
@@ -25,7 +26,7 @@
         (if-let [profile (create! db {:username username, :password password})]
           (created (resource-url profile)
                    (render-token profile secret))
-          (-> (response (render-message "username is already taken"))
+          (-> (response create-error)
               (assoc :status 409))))
 
       (GET "/:username" [username password]
@@ -41,6 +42,4 @@
       (DELETE "/:username" [username password]
         (when-authenticated db username password
                             (fn [_] (when (delete! db username)
-                                     (-> (render-message (str username
-                                                              " deleted"))
-                                         (response)))))))))
+                                     (response (deleted-message username)))))))))
--- src/zanmi/view/profile.clj
+++ src/zanmi/view/profile.clj
@@ -9,5 +9,15 @@
 (defn render-message [message]
   {:message message})
 
+(defn render-error [message]
+  {:error message})
+
 (defn render-token [profile secret]
   {:token (sign profile secret)})
+
+(def auth-error (render-error "bad username or password"))
+
+(def create-error (render-error "username is already taken"))
+
+(defn deleted-message [username]
+  (render-message (str username " deleted")))
