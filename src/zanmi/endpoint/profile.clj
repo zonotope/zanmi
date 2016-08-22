b69df01314ed90905ee9b472853a23d7e0f6720c
ben lamothe <zonotope@gmail.com>
2016-08-22 19:47:09 -0400

    extract response functions

--- src/zanmi/endpoint/profile.clj
+++ src/zanmi/endpoint/profile.clj
@@ -3,15 +3,38 @@
             [zanmi.view.profile :refer [auth-error deleted-message render-error
                                         render-token]]
             [compojure.core :refer [context DELETE GET PUT POST]]
-            [ring.util.response :refer [created response]]))
+            [ring.util.response :as response :refer [response]]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; url                                                                      ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (def ^:private route-prefix "/profiles")
 
 (defn resource-url [{username :username :as profile}]
   (str route-prefix "/" username))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; responses                                                                ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defn- ok [profile secret]
   (response (render-token profile secret)))
+
+(defn- created [profile secret]
+  (response/created (resource-url profile)
+                    (render-token profile secret)))
+
+(defn- deleted [username]
+  (response (deleted-message username)))
+
+(defn- error [e status]
+  (-> (response (render-error e))
+      (assoc :status status)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; auth                                                                     ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn- when-authenticated [db username password response-fn]
   (if-let [profile (authenticate db username password)]
@@ -19,16 +42,18 @@
     (-> (response auth-error)
         (assoc :status 401))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; endpoint                                                                 ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defn profile-endpoint [secret]
   (fn [{db :db :as endpoint}]
     (context route-prefix []
       (POST "/" [username password]
         (let [result (create! db {:username username, :password password})]
           (if-let [profile (:ok result)]
-            (created (resource-url profile)
-                     (render-token profile secret))
-            (-> (response (render-error (:error result)))
-                (assoc :status 409)))))
+            (created profile secret)
+            (error (:error result)))))
 
       (GET "/:username" [username password]
         (when-authenticated db username password
@@ -40,13 +65,9 @@
                               (let [result (update! db username new-password)]
                                 (if-let [new-profile (:ok result)]
                                   (ok new-profile secret)
-                                  (-> (:error result)
-                                      (render-error)
-                                      (response)
-                                      (assoc :status 400)))))))
+                                  (error (:error result)))))))
 
       (DELETE "/:username" [username password]
         (when-authenticated db username password
                             (fn [_] (when (delete! db username)
-                                     (-> (deleted-message username)
-                                         (response)))))))))
+                                     (deleted username))))))))
