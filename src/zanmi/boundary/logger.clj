4c1033226f726cb4f04e4be8e50da29b06576a41
ben lamothe <zonotope@gmail.com>
2016-09-02 00:32:27 -0400

    add throwable argument

--- src/zanmi/boundary/logger.clj
+++ src/zanmi/boundary/logger.clj
@@ -2,25 +2,32 @@
 
 (defprotocol Logger
   "Log messages"
-  (log [logger level message] "Log `message` to `logger`"))
+  (log [logger level throwable message]
+    "Log `message` to `logger` along with the trace from `throwable`"))
 
 (defn trace [logger message]
-  (log logger :trace message))
+  (log logger :trace nil message))
 
 (defn debug [logger message]
-  (log logger :debug message))
+  (log logger :debug nil message))
 
 (defn info [logger message]
-  (log logger :info message))
+  (log logger :info nil message))
 
 (defn warn [logger message]
-  (log logger :warn message))
+  (log logger :warn nil message))
 
-(defn error [logger message]
-  (log logger :error message))
+(defn error
+  ([logger message]
+   (log logger :error nil message))
+  ([logger throwable message]
+   (log logger :error throwable message)))
 
-(defn fatal [logger message]
-  (log logger :fatal message))
+(defn fatal
+  ([logger message]
+   (log logger :fatal nil message))
+  ([logger throwable message]
+   (log logger :fatal throwable message)))
 
 (defn report [logger message]
-  (log logger :report message))
+  (log logger :report nil message))
