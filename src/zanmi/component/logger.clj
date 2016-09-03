baa215a500ce079f898593b83e893f91215ee25a
ben lamothe <zonotope@gmail.com>
2016-09-02 21:02:30 -0400

    pass appender args as map

--- src/zanmi/component/logger.clj
+++ src/zanmi/component/logger.clj
@@ -14,7 +14,8 @@
   (log [logger level throwable message]
     (timbre/log* logger level throwable message)))
 
-(defn timbre-logger [{:keys [level path pattern] :as config}]
-  (map->TimbreLogger
-   {:level level
-    :appenders {:rolling (rolling-appender path pattern)}}))
+(defn timbre-logger [{:keys [level] :as config}]
+  (let [appender-config (select-keys config [:path :pattern])]
+    (map->TimbreLogger
+     {:level level
+      :appenders {:rolling (rolling-appender appender-config)}})))
