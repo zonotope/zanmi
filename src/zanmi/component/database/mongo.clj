cee0bf89aa78e12bf14060ebca05b305faa42bc9
ben lamothe <zonotope@gmail.com>
2016-09-05 11:11:38 -0400

    component/Lifecycle impl for Mongo

--- src/zanmi/component/database/mongo.clj
+++ src/zanmi/component/database/mongo.clj
@@ -1,9 +1,20 @@
-(ns zanmi.component.mongo
-  (:require [monger.core :as mongo])
+(ns zanmi.component.database.mongo
+  (:require [com.stuartsierra.component :as component]
+            [monger.core :as mongo])
   (:import [com.mongodb MongoOptions ServerAddress]))
 
 (defrecord Mongo []
-  )
+  component/Lifecycle
+  (start [mongo]
+    (if (:connection mongo)
+      mongo
+      (assoc mongo :connection (mongo/connect mongo))))
+
+  (stop [mongo]
+    (if-let [conn (:connection mongo)]
+      (do (mongo/disconnect conn)
+          (dissoc mongo :connection))
+      mongo)))
 
 (defn mongo [config]
   (map->Mongo config))
