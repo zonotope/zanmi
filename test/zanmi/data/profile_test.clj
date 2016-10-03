31e24f1c418bc2d28541857af38a832e61c487b2
ben lamothe <zonotope@gmail.com>
2016-10-02 20:48:34 -0400

    add test for authenticate

--- test/zanmi/data/profile_test.clj
+++ test/zanmi/data/profile_test.clj
@@ -1,1 +1,8 @@
-(ns zanmi.data.profile-test)
+(ns zanmi.data.profile-test
+  (:require [zanmi.data.profile :as profile]
+            [clojure.test :refer :all]))
+
+(deftest test-authenticate
+  (let [profile (profile/build {:username "test-user", :password "correct"})]
+    (is (= (profile/authenticate profile "correct") profile))
+    (is (nil? (profile/authenticate profile "wrong")))))
