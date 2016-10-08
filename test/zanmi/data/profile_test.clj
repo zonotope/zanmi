7de842e1195023b078193dd979fa7eed1933af8b
ben lamothe <zonotope@gmail.com>
2016-10-08 01:48:24 -0400

    use let

--- test/zanmi/data/profile_test.clj
+++ test/zanmi/data/profile_test.clj
@@ -3,68 +3,68 @@
             [zanmi.test-config :refer [config]]
             [clojure.test :refer :all]))
 
-(def schema (:schema (profile-repo (:profile-repo config))))
+(let [schema (:schema (profile-repo (:profile-repo config)))]
 
-(deftest test-authenticate
-  (testing "authenticate"
-    (let [profile (hash-password (with-id {:username "test-user"
-                                           :password "correct"}))]
-      (testing "with the correct password"
-        (is (= (authenticate profile "correct")
-               profile)
-            "returns the profile"))
+  (deftest test-authenticate
+    (testing "authenticate"
+      (let [profile (hash-password (with-id {:username "test-user"
+                                             :password "correct"}))]
+        (testing "with the correct password"
+          (is (= (authenticate profile "correct")
+                 profile)
+              "returns the profile"))
 
-      (testing "with the wrong password"
-        (is (nil? (authenticate profile "wrong"))
-            "returns nil")))))
+        (testing "with the wrong password"
+          (is (nil? (authenticate profile "wrong"))
+              "returns nil")))))
 
-(deftest test-create
-  (testing "create"
-    (testing "with valid attributes"
-      (let [profile (:ok (create schema {:username "tester"
-                                         :password "this is only a test"}))]
-        (is (not (nil? profile))
-            "returns the profile")
-
-        (testing "id"
-          (let [id (:id profile)]
-            (is (not (nil? id))
-                "is included")
-
-            (is (uuid? id)
-                "is a uuid")))
-
-        (is (nil? (:password profile))
-            "doesn't include the raw password")
-
-        (is (not (nil? :hashed-password))
-            "includes the hashed password")))
-
-    (testing "with invalid attributes"
-      (let [{profile :ok error :error} (create schema {:username "tester"
-                                                       :password "p4$$w0rd"})]
-        (is (not (nil? error))
-            "returns an error")
-
-        (is (nil? profile)
-            "does not return the profile")))))
-
-(deftest test-update
-  (testing "update"
-    (let [profile (:ok (create schema {:username "tester"
-                                       :password "this is only a test"}))]
-      (testing "with a valid new password"
-        (let [updated (:ok (update schema profile "this really is a test"))]
-          (is (not (nil? updated))
+  (deftest test-create
+    (testing "create"
+      (testing "with valid attributes"
+        (let [profile (:ok (create schema {:username "tester"
+                                           :password "this is only a test"}))]
+          (is (not (nil? profile))
               "returns the profile")
 
-          (is (nil? (:username updated))
-              "doesn't include the username")
+          (testing "id"
+            (let [id (:id profile)]
+              (is (not (nil? id))
+                  "is included")
 
-          (is (not (= (:hashed-password profile) (:hashed-pasword updated)))
-              "includes a different hashed password")))
+              (is (uuid? id)
+                  "is a uuid")))
 
-      (testing "with an invalid new password"
-        (let [error (:error (update schema profile "p4$$w0rd"))]
+          (is (nil? (:password profile))
+              "doesn't include the raw password")
+
+          (is (not (nil? :hashed-password))
+              "includes the hashed password")))
+
+      (testing "with invalid attributes"
+        (let [{profile :ok error :error} (create schema {:username "tester"
+                                                         :password "p4$$w0rd"})]
           (is (not (nil? error))
-              "returns an error"))))))
+              "returns an error")
+
+          (is (nil? profile)
+              "does not return the profile")))))
+
+  (deftest test-update
+    (testing "update"
+      (let [profile (:ok (create schema {:username "tester"
+                                         :password "this is only a test"}))]
+        (testing "with a valid new password"
+          (let [updated (:ok (update schema profile "this really is a test"))]
+            (is (not (nil? updated))
+                "returns the profile")
+
+            (is (nil? (:username updated))
+                "doesn't include the username")
+
+            (is (not (= (:hashed-password profile) (:hashed-pasword updated)))
+                "includes a different hashed password")))
+
+        (testing "with an invalid new password"
+          (let [error (:error (update schema profile "p4$$w0rd"))]
+            (is (not (nil? error))
+                "returns an error")))))))
