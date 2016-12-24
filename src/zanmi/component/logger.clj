3c0f172358772f9844d9d059c2153924dadc9536
ben lamothe <zonotope@gmail.com>
2016-12-23 19:22:06 -0500

    remove redundant component implementation

--- src/zanmi/component/logger.clj
+++ src/zanmi/component/logger.clj
@@ -1,15 +1,10 @@
 (ns zanmi.component.logger
   (:require [zanmi.boundary.logger :as logger]
-            [com.stuartsierra.component :as component]
             [taoensso.timbre :as timbre]
             [taoensso.timbre.appenders.3rd-party.rolling
              :refer [rolling-appender]]))
 
 (defrecord Timbre []
-  component/Lifecycle
-  (start [logger] logger)
-  (stop [logger] logger)
-
   logger/Logger
   (log [logger level throwable message]
     (timbre/log* logger level throwable message)))
