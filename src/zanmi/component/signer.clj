1d54fccbe4f8ac2f944690e58ca32d76b8bc0ce3
ben lamothe <zonotope@gmail.com>
2016-12-25 23:13:10 -0500

    add ecdsa signer

--- src/zanmi/component/signer.clj
+++ src/zanmi/component/signer.clj
@@ -1,8 +1,10 @@
 (ns zanmi.component.signer
-  (:require [zanmi.component.signer.rsa-pss :refer [rsa-pss-signer]]
+  (:require [zanmi.component.signer.ecdsa :refer [ecdsa-signer]]
+            [zanmi.component.signer.rsa-pss :refer [rsa-pss-signer]]
             [zanmi.component.signer.sha :refer [sha-signer]]))
 
 (defn signer [config]
   (case (:algorithm config)
+    :ecdsa (ecdsa-signer config)
     :rsa-pss (rsa-pss-signer config)
     :sha (sha-signer config)))
